7291c3391dac4ee21bf74ac7fe476d20dcd923c3
Maksym <maksym.045z@gmail.com>
2025-05-08 13:13:28 +0200

    first commit

---
+++ src/main/java/org/bigxp/bigxp/BigXpApplication.java
@@ -0,0 +1,13 @@
+package org.bigxp.bigxp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigXpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigXpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/bigxp/bigxp/BigXpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bigxp.bigxp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigXpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
