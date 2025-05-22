0a77cc083f91a0061d61e2ed701ca2db2b4b6fbb
Maksym <maksym.045z@gmail.com>
2025-05-22 10:55:55 +0200

    activity update

--- src/main/java/org/bigxp/bigxp/Controller/BookingRestController.java
+++ src/main/java/org/bigxp/bigxp/Controller/BookingRestController.java
@@ -5,6 +5,7 @@ import org.bigxp.bigxp.Repository.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import java.util.List;
 //redigere booking(intern system personale da de skal administrer det)
 //slet booking(intern system personale da de skal administrer det)
 //Bushra laver det
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/booking")
 public class BookingRestController {
